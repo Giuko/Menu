69020736122e62aeb5b8033e305b155ca14dea92
Giuko <56999735+Giuko@users.noreply.github.com>
2022-01-26 17:55:08 +0100

    Create test.c

---
+++ test.c
@@ -0,0 +1,11 @@
+/*
+  Giuseppe Maganuco Francesco Vincenzo's property
+*/
+
+#include <stdio.h>
+#include "miniMenu.h"
+int main(void){
+	int c;
+	c = menu("PROVA", "Prova 1", "Prova 2", "Prova 3", "Prova 4", "Prova 5");
+	printf("%d", c);
+}
